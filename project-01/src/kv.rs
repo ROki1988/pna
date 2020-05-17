3c910ef58801422c2a0937621efb235ae434e67b
ROki1988 <roki.swindler@gmail.com>
2020-05-17 21:15:37 +0900

    add docs

--- project-01/src/kv.rs
+++ project-01/src/kv.rs
@@ -1,20 +1,25 @@
 use std::collections::HashMap;
 
+/// key value store
 pub struct KvStore(HashMap<String, String>);
 
 impl KvStore {
+    /// Return new store
     pub fn new() -> Self {
         Self(HashMap::new())
     }
 
+    /// Get value by key
     pub fn get(&self, key: String) -> Option<String> {
         self.0.get(&key).map(String::to_owned)
     }
 
+    /// Set value with key
     pub fn set(&mut self, key: String, value: String) {
         self.0.insert(key, value);
     }
 
+    /// Remove key-value
     pub fn remove(&mut self, key: String)  {
         self.0.remove(&key);
     }
--- project-01/src/lib.rs
+++ project-01/src/lib.rs
@@ -1,3 +1,13 @@
+#![deny(missing_docs)]
+
+//! use kv::KvStore;
+//!
+//! fn main() {
+//!     let mut store = KvStore::new();
+//!     store.set("key".to_owned(), "value".to_owned());
+//!     assert_eq!(store.get("key".to_owned()), Some("value".to_owned()));
+//! }
+//!
 pub use kv::KvStore;
 
 mod kv;
